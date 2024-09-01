75f5922d1b1ee5218e71c004e6a5d7a0010568c3
Simon <simon.vereecke@live.com>
2024-09-01 07:16:35 +0200

    reposition button

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,16 @@ function roll(event: MouseEvent) {
 </script>
 
 <template>
-  <main class="container">
-    <p class="id">ADVICE #{{ `117` }}</p>
+  <main>
+    <div class="container">
+      <p class="id">ADVICE #{{ `117` }}</p>
 
-    <div class="advice">
-      &#8220;It is easy to sit up and take notice, what's difficult is getting up and taking
-      action.&#8221;
+      <div class="advice">
+        &#8220;It is easy to sit up and take notice, what's difficult is getting up and taking
+        action.&#8221;
+      </div>
+      <img src="/images/pattern-divider-mobile.svg" />
     </div>
-    <img src="/images/pattern-divider-mobile.svg" />
     <button @click="roll">
       <img src="/images/icon-dice.svg" />
     </button>
@@ -36,11 +38,16 @@ function roll(event: MouseEvent) {
 <style lang="sass" scoped>
 @import './assets/sass/_variables.sass'
 
+main
+  display: flex
+  flex-direction: column
+  align-items: center
+
 button
   display: inline-block
   background-color: $secondary-color
   padding: 1.25rem
-  margin-top: 2rem
+  margin-top: -2rem
   line-height: 0
   border: none
   border-radius: 100%
@@ -56,10 +63,6 @@ header
   margin: 0rem 1.7rem
   letter-spacing: 0px
   margin-bottom: 1.5rem
-
-  // &::before
-  //   content: '\275D'
-  //   margin-right: 0px
 
   @media (min-width: 1024px)
     font-size: $font-size
@@ -85,22 +88,4 @@ header
   letter-spacing: 3.7px
   font-weight: 600
   font-size: 11px
-
-.logo
-  display: block
-  margin: 0 auto 2rem
-
-@media (min-width: 1024px)
-  header
-    display: flex
-    place-items: center
-    padding-right: calc(var(--section-gap) / 2)
-
-  .logo
-    margin: 0 2rem 0 0
-
-  header .wrapper
-    display: flex
-    place-items: flex-start
-    flex-wrap: wrap
 </style>
