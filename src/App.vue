a46424c12eb3539e2cdf5119a77c756fb0855e80
Simon <simon.vereecke@live.com>
2024-09-04 11:16:55 +0200

    extract Advice in separate component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Advice from './components/Advice.vue'
 import DiceButton from './components/DiceButton.vue'
 import PatternDivider from './components/PatternDivider.vue'
 import { useFetch } from './hooks/useFetch'
@@ -14,29 +13,17 @@ interface Slip {
   advice: string
 }
 
-const { data, error, reload } = useFetch<Response>('https://api.adviceslip.com/advice')
-
-function roll() {
-  reload()
-}
+const { data, reload } = useFetch<Response>('https://api.adviceslip.com/advice')
 </script>
 
 <template>
   <main>
     <div class="container">
-      <p class="id">ADVICE #{{ data && data.slip.id }}</p>
-      <div class="advice">&#8220;{{ data && data.slip.advice }}&#8221;</div>
+      <Advice :id="data?.slip.id" :message="data?.slip.advice" />
       <PatternDivider />
     </div>
-    <DiceButton @click="roll" />
+    <DiceButton @click="reload" />
   </main>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>-->
 </template>
 
 <style lang="sass" scoped>
@@ -46,22 +33,6 @@ main
   display: flex
   flex-direction: column
   align-items: center
-
-header
-  line-height: 1.5
-
-.advice
-  color: $primary-color
-  font-size: 23px
-  text-align: center
-  line-height: 2.05rem
-  margin: 0rem 1.7rem
-  letter-spacing: 0px
-
-  @media (min-width: 1024px)
-    font-size: $font-size
-    line-height: 1.35
-    margin: 0rem 2rem
 
 .container
   display: flex
@@ -78,16 +49,4 @@ header
   @media (min-width: 1024px)
     margin-top: 13.75em
     border-radius: 20px
-
-.id
-  color: $secondary-color
-  text-align: center
-  padding: 2.5rem 2.5rem 1.4rem 2.5rem
-  letter-spacing: 3.7px
-  font-weight: 600
-  font-size: 11px
-
-  @media (min-width: 1024px)
-    font-size: 14px
-    padding-top: 3rem
 </style>
---
+++ src/components/Advice.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  id: number | undefined
+  message: string | undefined
+}>()
+</script>
+
+<template>
+  <p class="id">ADVICE #{{ id }}</p>
+  <div class="advice">&#8220;{{ message }}&#8221;</div>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/sass/_variables.sass'
+
+.id
+  color: $secondary-color
+  text-align: center
+  padding: 2.5rem 2.5rem 1.4rem 2.5rem
+  letter-spacing: 3.7px
+  font-weight: 600
+  font-size: 11px
+
+  @media (min-width: 1024px)
+    font-size: 14px
+    padding-top: 3rem
+
+.advice
+  color: $primary-color
+  font-size: 23px
+  text-align: center
+  line-height: 2.05rem
+  margin: 0rem 1.7rem
+  letter-spacing: 0px
+
+  @media (min-width: 1024px)
+    font-size: $font-size
+    line-height: 1.35
+    margin: 0rem 2rem
+</style>
